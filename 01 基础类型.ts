887d82266b2dedc946811123c3d7fdf7314675f0
Sang-Sang33 <2363481389@qq.com>
2020-09-03 21:51:41 +0800

    1

---
+++ 01 基础类型.ts
@@ -0,0 +1,20 @@
+//number类型
+let num: number = 123;
+
+//字符串类型
+let str: string = 'fasf';
+str = 'fsagggdg';
+
+//布尔类型
+let flag: boolean = false;
+flag = true;
+
+//undefined
+let u: undefined = undefined;
+
+//null
+let n: null = null;
+let nu: number = null;
+
+let add: string = num + str;
+console.log(add);
---
+++ 02 数组.ts
@@ -0,0 +1,19 @@
+//数字的数组
+let ar: number[] = [1, 2, 3, 5, 6, 6, 4, 4];
+let arr: Array<number> = [1, 2, 3, 5, 6, 6, 4, 4];
+
+//字符串的数组
+let strArr: string[] = ['sf', 'sa', 'sgs', 'agg'];
+let stArr: Array<string> = ['sf', 'fsfsf', 'gggg', 'fsfs'];
+
+//布尔的数组
+let flagAr: boolean[] = [true, false];
+
+//不确定类型的数组
+let notSureArr: any[] = [12313, 'sfa', false];
+let notArr: Array<any> = [false, 'fsfs', 123132];
+
+let catArr: number[] = ar.concat(arr);
+console.log(catArr);
+let index: number = catArr.indexOf(4);
+console.log(index);
