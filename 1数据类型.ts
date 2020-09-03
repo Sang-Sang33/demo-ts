b794fa575f2c13a5f92a7486fb63e0fe9fe52158
Sang-Sang33 <2363481389@qq.com>
2020-09-03 21:51:41 +0800

    1

---
+++ 1数据类型.ts
@@ -0,0 +1,6 @@
+let num: number = 123;
+let str: string = 'fasf';
+let flag: boolean = false;
+let arr: number[] = [1, 2, 22, 33, 36];
+let ar: Array<string> = ['sf', 'fsa', 'fs'];
+let x: [boolean, string, number] = [true, 'fas', 44];
